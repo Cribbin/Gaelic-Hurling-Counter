414027d40fbaa22bff15e058a5e6e8b0f378c162
PJ Cribbin <CribbinPJ@gmail.com>
2015-07-30 18:35:54 +0100

    And intro instructions toast

--- app/src/main/java/com/pjcribbin/gaelichurlingcounter/MainActivity.java
+++ app/src/main/java/com/pjcribbin/gaelichurlingcounter/MainActivity.java
@@ -17,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast infoToast = Toast.makeText(this, "Tap a goals or points number to increment it!", Toast.LENGTH_LONG);
+        infoToast.show();
     }
 
     @Override
