7811fb3120066fe6f562ac34680d6f3fdf6910b9
PJ Cribbin <CribbinPJ@gmail.com>
2015-07-30 15:25:15 +0100

    Add onClick to buttons

--- app/src/main/java/com/pjcribbin/gaelichurlingcounter/MainActivity.java
+++ app/src/main/java/com/pjcribbin/gaelichurlingcounter/MainActivity.java
@@ -4,9 +4,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
+    int team1Goals = 0, team1Points = 0, team1Score = 0;
+    int team2Goals = 0, team2Points = 0, team2Score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +38,49 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void incrementTeam1Goals(View v){
+        team1Goals++;
+        team1Score += 3;
+        updateTeam1();
+    }
+
+    public void incrementTeam1Points(View v){
+        team1Points++;
+        team1Score++;
+        updateTeam1();
+    }
+
+    public void incrementTeam2Goals(View v){
+        team2Goals++;
+        team2Score += 3;
+        updateTeam2();
+    }
+
+    public void incrementTeam2Points(View v){
+        team2Points++;
+        team2Score++;
+        updateTeam2();
+    }
+
+    public void updateTeam1(){
+        TextView team1GoalsTextView = (TextView) findViewById(R.id.team_1_goals);
+        TextView team1PointsTextView = (TextView) findViewById(R.id.team_1_points);
+        TextView team1ScoreTextView = (TextView) findViewById(R.id.team_1_score);
+
+        team1GoalsTextView.setText(String.valueOf(team1Goals));
+        team1PointsTextView.setText(String.valueOf(team1Points));
+        team1ScoreTextView.setText(String.valueOf(team1Score));
+    }
+
+    public void updateTeam2(){
+        TextView team2GoalsTextView = (TextView) findViewById(R.id.team_2_goals);
+        TextView team2PointsTextView = (TextView) findViewById(R.id.team_2_points);
+        TextView team2ScoreTextView = (TextView) findViewById(R.id.team_2_score);
+
+        team2GoalsTextView.setText(String.valueOf(team2Goals));
+        team2PointsTextView.setText(String.valueOf(team2Points));
+        team2ScoreTextView.setText(String.valueOf(team2Score));
     }
 }
